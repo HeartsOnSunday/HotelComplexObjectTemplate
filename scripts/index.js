bc535d00f667c586b08731b1f351e7611faadc44
Leah <rhodycodes401@gmail.com>
2017-11-21 01:02:51 +0000

    RadioBTNs

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,32 @@ var hotel = {
     name: "CareerDevs Innt"
 }
 
+// console.log(hotel.rooms[1].price);
+
+//this loop accounts for new items you havent written yet!
+
+
+// console.log(document.getElementById("radio").nodeName);
+// console.log(document.getElementById("label").nodeName);
+
+
+
+for (var i = 0; i < hotel.rooms.length; i++) {
+    //crate radio button
+    var radioBtn = document.createElement("INPUT");
+    // create attributes
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "roomType");
+    radioBtn.setAttribute("value", i);
+    radioBtn.setAttribute("id", "room" + i);
+    //create Label
+    var radioLbl = document.createElement("LABEL");
+    //create attributes
+    radioLbl.innerHTML = hotel.rooms[i].name;
+    document.getElementById("radialSection").appendChild(radioBtn);
+    document.getElementById("radialSection").appendChild(radioLbl);
+
+}
+
+
+// hotel.rooms[i]
